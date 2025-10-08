f178089c27d3021799948941190480112a90dcca
Nima Karkhaneh <nimakarkhaneh@gmail.com>
2025-10-08 14:09:33 +1100

    Create the first component - CourseGoal.tsx

---
+++ react-ts-basics/src/components/CourseGoal.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const CourseGoal = ({ title, description } : {
+    title: string,
+    description: string
+}) => {
+  return (
+    <article>
+        <div>
+            <h2>{title}</h2>
+            <p>{description}</p>
+        </div>
+        <button>Delete</button>
+    </article>
+  )
+}
+
+export default CourseGoal
