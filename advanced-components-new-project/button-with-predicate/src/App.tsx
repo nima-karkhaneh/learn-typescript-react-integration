63aad5fffb5ed4e3308e9a7d9d4593421eb802fe
Nima Karkhaneh <nimakarkhaneh@gmail.com>
2025-10-21 00:55:40 +1100

    Add type predicate to Button.tsx

--- advanced-components-new-project/button-with-predicate/src/App.tsx
+++ advanced-components-new-project/button-with-predicate/src/App.tsx
@@ -5,14 +5,12 @@ function App() {
 
   return (
     <main>
-      <main>
       <p>
         <Button el='button'>A Button</Button>
       </p>
       <p>
         <Button el='anchor' href='https://google.com'>A link</Button>
       </p>
-    </main>
     </main>
   )
 }
