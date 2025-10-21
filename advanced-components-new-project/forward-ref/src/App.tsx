622e2f04d5701a2af081e7fda6e4971a1e523ebf
Nima Karkhaneh <nimakarkhaneh@gmail.com>
2025-10-22 00:53:46 +1100

    Add forward ref to Input.tsx

---
+++ advanced-components-new-project/forward-ref/src/App.tsx
@@ -0,0 +1,17 @@
+import { useRef } from 'react'
+import Button from './components/Button'
+import Container from './components/Container'
+import Input from './components/Input'
+
+function App() {
+  const input = useRef<HTMLInputElement>(null)
+
+
+  return (
+    <main>
+      <Input label='Test' id='test' ref={input} />
+    </main>
+  )
+}
+
+export default App
