29bcf5804e87dcb3f393d7f5058b61bcf78c9bd5
Nima Karkhaneh <nimakarkhaneh@gmail.com>
2025-11-14 22:17:59 +1100

    Set up a basic timer with a progress bar

--- advanced-state-managment/src/components/Timer.tsx
+++ advanced-state-managment/src/components/Timer.tsx
@@ -1,11 +1,28 @@
+import { useEffect, useState } from 'react';
 import { type Timer } from '../store/timers-context.tsx';
 import Container from './UI/Container.tsx';
 
 export default function Timer({ name, duration }: Timer) {
+  const [remainingTime, setRemainingTime] = useState(duration * 1000)
+
+
+  useEffect(() => {
+    setInterval(function() {
+      setRemainingTime(prevTime => prevTime - 50)
+    }, 50)
+  }, []
+  )
+
+  const formattedRemainingTime = (remainingTime / 1000).toFixed(2)
+  
   return (
+    
     <Container as="article">
       <h2>{name}</h2>
-      <p>{duration}</p>
+      <p>
+        <progress value={remainingTime} max={duration * 1000}></progress>
+      </p>
+      <p>{formattedRemainingTime}</p>
     </Container>
   );
 }
