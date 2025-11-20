b92490c8af8c07a65d95cae8f24e1ff936d7d7e5
Nima Karkhaneh <nimakarkhaneh@gmail.com>
2025-11-20 13:19:01 +1100

    Enable button functionality to start/stop timers

--- advanced-state-managment/src/components/Timer.tsx
+++ advanced-state-managment/src/components/Timer.tsx
@@ -1,25 +1,37 @@
 import { useEffect, useRef, useState } from 'react';
-import { type Timer } from '../store/timers-context.tsx';
+import { useTimersContext, type Timer } from '../store/timers-context.tsx';
 import Container from './UI/Container.tsx';
 
 export default function Timer({ name, duration }: Timer) {
 
   const interval = useRef<number | null>(null)
   const [remainingTime, setRemainingTime] = useState(duration * 1000)
+  const { isRunning } = useTimersContext();
 
   if (remainingTime <=0 && interval.current) {
     clearInterval(interval.current)
   }
 
-
   useEffect(() => {
-    const timer = setInterval(function() {
-      setRemainingTime(prevTime => prevTime - 50)
-    }, 50)
-    interval.current = timer
-    return () => clearInterval(timer)
-  }, []
-  )
+    let timer: number;
+    
+    if (isRunning) {
+      timer = setInterval(function () {
+        setRemainingTime((prevTime) => {
+  if (prevTime <= 0) {
+    return prevTime;
+  }
+  return prevTime - 50;
+});
+      }, 50);
+      interval.current = timer;
+
+    } else if (interval.current) {
+      clearInterval(interval.current)
+    }
+
+    return () => clearInterval(timer);
+  }, [isRunning]);
 
   const formattedRemainingTime = (remainingTime / 1000).toFixed(2)
   
