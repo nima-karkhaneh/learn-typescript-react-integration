ab7908a54ae28fcd7d9b17651ed39513dc034542
Nima Karkhaneh <nimakarkhaneh@gmail.com>
2025-11-24 23:51:12 +1100

    Make fetch functionality work in App.tsx

--- fetch-data/src/App.tsx
+++ fetch-data/src/App.tsx
@@ -1,5 +1,47 @@
-function App() {
-  return <h1>Data Fetching!</h1>;
+import { useEffect, useState, type ReactNode } from "react";
+import { get } from "./util/https";
+import BlogPosts, { type BlogPost } from "./components/BlogPosts";
+import fetchingImg from './assets/data-fetching.png';
+
+type RawDataBlogPosts = {
+  id: number,
+  title: string,
+  body: string,
+  userId: number
 }
 
+
+
+function App() {
+  const [fetchedPost, setFetchedPost] = useState<BlogPost[] | null>(null)
+  useEffect(() => {
+    async function fetchPost() {
+      const data = await get(
+        'https://jsonplaceholder.typicode.com/posts'
+      ) as RawDataBlogPosts[]
+
+      const blogPost: BlogPost[] = data.map(post => {
+      return {
+        id: post.id,
+        title:post.title,
+        text:post.body
+      }
+    })
+    setFetchedPost(blogPost)
+    };
+    fetchPost()
+  }, [])
+
+  let content: ReactNode
+  if (fetchedPost) {
+    content = <BlogPosts posts={fetchedPost} />
+  }
+
+  return(
+    <main>
+      <img src={fetchingImg} alt="" />
+      {content}
+    </main>
+  )
+}
 export default App;
